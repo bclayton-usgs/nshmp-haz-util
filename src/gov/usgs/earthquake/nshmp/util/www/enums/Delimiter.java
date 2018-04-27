9d37430dc18df0e2f5f89b9eccf32d7af3cfa7c2
bclayton-usgs <bclayton@usgs.gov>
2018-04-27 14:10:03 -0600

    Enums of delimiters

---
+++ src/gov/usgs/earthquake/nshmp/util/www/enums/Delimiter.java
@@ -0,0 +1,70 @@
+package gov.usgs.earthquake.nshmp.util.www.enums;
+
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+
+/**
+ * Delimiter identifiers, each of which can provide a {@link Joiner} and
+ * {@link Splitter}.
+ * 
+ * @author Peter Powers
+ */
+public enum Delimiter {
+
+  /** Colon (':') delimiter. */
+  COLON(':'),
+
+  /** Comma (',') delimiter. */
+  COMMA(','),
+
+  /** Dash ('-') delimiter. */
+  DASH('-'),
+
+  /** Period ('.') delimiter. */
+  PERIOD('.'),
+
+  /** Forward-slash ('/') delimiter. */
+  SLASH('/'),
+
+  /**
+   * Whitespace (' ') delimiter.
+   * @see CharMatcher#WHITESPACE
+   */
+  SPACE(' ', CharMatcher.WHITESPACE),
+
+  /** Underscore ('_') delimiter. */
+  UNDERSCORE('_');
+
+  private Joiner joiner;
+  private Splitter splitter;
+
+  private Delimiter(char separator) {
+    joiner = Joiner.on(separator).skipNulls();
+    splitter = Splitter.on(separator).omitEmptyStrings().trimResults();
+  }
+
+  private Delimiter(char joinSeparator, CharMatcher splitMatcher) {
+    joiner = Joiner.on(joinSeparator).skipNulls();
+    splitter = Splitter.on(splitMatcher).omitEmptyStrings().trimResults();
+  }
+
+  /**
+   * Returns a null-skipping {@link Joiner} on this {@code Delimiter}.
+   * @see Joiner#skipNulls()
+   */
+  public Joiner joiner() {
+    return joiner;
+  }
+
+  /**
+   * Returns an empty-string-omitting and result-trimming {@link Splitter} on
+   * this {@code Delimiter}.
+   *
+   * @see Splitter#omitEmptyStrings()
+   * @see Splitter#trimResults()
+   */
+  public Splitter splitter() {
+    return splitter;
+  }
+}
